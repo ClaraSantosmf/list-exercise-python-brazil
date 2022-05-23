0076723eedf6b52f74b41c03d4f7ae0f442ee32c
ClaraSantosmf <clarasantosmf@gmail.com>
2022-05-23 18:49:04 -0300

    Exercise 01 and 02 concluded

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -13,3 +13,5 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 def escreva_um_numero():
     """Escreva aqui em baixo a sua solução"""
+    a = int(input('Diz um número ai'))
+    print('O número informado foi', a)
