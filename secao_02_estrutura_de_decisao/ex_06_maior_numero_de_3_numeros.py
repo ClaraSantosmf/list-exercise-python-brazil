14abf8f8f68518c3221725636511aa2958e34b26
ClaraSantosmf <clarasantosmf@gmail.com>
2022-05-26 14:57:52 -0300

    Exercise from 01 to 14

--- secao_02_estrutura_de_decisao/ex_06_maior_numero_de_3_numeros.py
+++ secao_02_estrutura_de_decisao/ex_06_maior_numero_de_3_numeros.py
@@ -17,3 +17,6 @@ Faça um Programa que leia três números e mostre o maior deles.
 
 def calcular_maior_de_3_numeros(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+    a = sorted([x, y, z])
+    b = a.pop()
+    print(b)
