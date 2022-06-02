f437f40aaa04a5bc4cdf40f3357d07b839083ee0
ClaraSantosmf <clarasantosmf@gmail.com>
2022-06-02 19:02:17 -0300

    Solution to 19 exercise section 03

--- secao_03_estrutura_de_repeticao/ex_19_estatisticas_de_n_numeros_com_limitacao.py
+++ secao_03_estrutura_de_repeticao/ex_19_estatisticas_de_n_numeros_com_limitacao.py
@@ -14,7 +14,7 @@ Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
     'Somente números de 0 a 1000 são permitidos'
     >>> calcular_estatisticas(1, 2, -10)
     'Somente números de 0 a 1000 são permitidos'
-    >>> calcular_estatisticas(1, 2, 1001)
+    >>> calcular_estatisticas(1, 2, 100.5)
     'Somente números de 0 a 1000 são permitidos'
     >>> calcular_estatisticas(1, 2, 1198)
     'Somente números de 0 a 1000 são permitidos'
@@ -24,3 +24,12 @@ Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 
 def calcular_estatisticas(*numeros) -> str:
     """Escreva aqui em baixo a sua solução"""
+    if len(numeros) == 0:
+        return 'Maior valor: não existe. Menor valor: não existe. Soma: 0'
+    intervalo_valido = set(range(0, 1001))
+    essa_lista_deve_ser_vazia = set(numeros).difference(intervalo_valido)
+    if len(essa_lista_deve_ser_vazia) == 0:
+        listinha = sorted(numeros)
+        soma = sum(listinha)
+        return f'Maior valor: {listinha[-1]}. Menor valor: {listinha[0]}. Soma: {soma}'
+    return 'Somente números de 0 a 1000 são permitidos'
